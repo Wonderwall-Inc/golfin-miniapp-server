79688b8824457f80e0db71f1d7211ebdd13002c7
wonderwall-tim <timothy.chow@wonderwall-g.gmail.com>
2024-09-17 21:27:07 +0900

    cors

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://telegram.golfin.dev"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
