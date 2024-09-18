a2f35838ab701d87dfb8b4b1fab2bda168418ce3
wonderwall-tim <timothy.chow@wonderwall-g.gmail.com>
2024-09-19 02:39:09 +0900

    main, vercel

--- main.py
+++ main.py
@@ -30,7 +30,8 @@ origins = [
 ]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    # allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
