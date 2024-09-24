8f81a370483e9c24346036fb44aa8bd1467e0805
wonderwall-tim <timothy.chow@wonderwall-g.gmail.com>
2024-09-24 18:48:16 +0900

    revert record

--- app/record/api/v1/record.py
+++ app/record/api/v1/record.py
@@ -1,34 +1,34 @@
-"""Record App API Routes"""
+# """Record App API Routes"""
 
-from typing import List, Optional
-from fastapi import APIRouter, Depends, Query
-from sqlalchemy.orm import Session
+# from typing import List, Optional
+# from fastapi import APIRouter, Depends, Query
+# from sqlalchemy.orm import Session
 
-from core import database
-from app.record import schemas
-from app.record.api.v1 import service
-
-
-get_db = database.get_db
-
-router = APIRouter(prefix="/api/v1/record", tags=["record"])
-
-@router.post("/create", response_model=schemas.RecordCreateResponseSchema)
-def create_record(request: schemas.RecordCreateRequestSchema, db: Session = Depends(get_db)):
-    """Create Record"""
-    return service.create_record(request, db)
+# from core import database
+# from app.record import schemas
+# from app.record.api.v1 import service
 
 
-@router.get("/detail", response_model=schemas.RecordRetrievalResponseSchema)
-def get_record_detail(id: Optional[int] = None, user_id: Optional[int] = None,db: Session = Depends(get_db)):
-    """Retrieve Record Details from Single User"""
-    return service.retrieve_record(id, user_id, db)
+# get_db = database.get_db
+
+# router = APIRouter(prefix="/api/v1/record", tags=["record"])
+
+# @router.post("/create", response_model=schemas.RecordCreateResponseSchema)
+# def create_record(request: schemas.RecordCreateRequestSchema, db: Session = Depends(get_db)):
+#     """Create Record"""
+#     return service.create_record(request, db)
 
 
-@router.get("/details", response_model=List[schemas.RecordRetrievalResponseSchema])
-def get_details(user_ids: List[int] = Query(default=None), skip: int = 0, limit: int = 15, db: Session = Depends(get_db)):
-    """Retrieve Records by List from Multiple Users"""
-    return service.retrieve_record_list(db, user_ids, skip, limit)
+# @router.get("/detail", response_model=schemas.RecordRetrievalResponseSchema)
+# def get_record_detail(id: Optional[int] = None, user_id: Optional[int] = None,db: Session = Depends(get_db)):
+#     """Retrieve Record Details from Single User"""
+#     return service.retrieve_record(id, user_id, db)
+
+
+# @router.get("/details", response_model=List[schemas.RecordRetrievalResponseSchema])
+# def get_details(user_ids: List[int] = Query(default=None), skip: int = 0, limit: int = 15, db: Session = Depends(get_db)):
+#     """Retrieve Records by List from Multiple Users"""
+#     return service.retrieve_record_list(db, user_ids, skip, limit)
 
 
 # @router.put("/update", response_model=schemas.RecordUpdateResponseSchema)
--- app/record/models.py
+++ app/record/models.py
@@ -1,57 +1,57 @@
-"""Record app DB models"""
+# """Record app DB models"""
 
-from datetime import datetime
-from sqlalchemy import Integer, ForeignKey, DateTime, JSON, Enum
-from sqlalchemy.orm import mapped_column, Mapped, relationship
-from core.database import Base
-from typing import Optional, Literal, get_args
-
-
-RecordActionType = Literal["GET", "LIST", "CREATE", "UPDATE", "BATCH_UPDATE", ]
-
-TableType = Literal["USER", "POINT", "ACTIVITY", "FRIEND", "SOCIAL_MEDIA", "GAME_CHARACTER", "RECORD"]
+# from datetime import datetime
+# from sqlalchemy import Integer, ForeignKey, DateTime, JSON, Enum
+# from sqlalchemy.orm import mapped_column, Mapped, relationship
+# from core.database import Base
+# from typing import Optional, Literal, get_args
 
 
+# RecordActionType = Literal["GET", "LIST", "CREATE", "UPDATE", "BATCH_UPDATE", ]
 
-class RecordModel(Base):
-    __tablename__ = "record"
-    id: Mapped[int] = mapped_column(
-        Integer,
-        primary_key=True,
-        unique=True,
-        index=True,
-        nullable=False,
-        autoincrement=True,
-    )
-    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("user.id"),  nullable=False, unique=False)
+# TableType = Literal["USER", "POINT", "ACTIVITY", "FRIEND", "SOCIAL_MEDIA", "GAME_CHARACTER", "RECORD"]
+
+
+
+# class RecordModel(Base):
+#     __tablename__ = "record"
+#     id: Mapped[int] = mapped_column(
+#         Integer,
+#         primary_key=True,
+#         unique=True,
+#         index=True,
+#         nullable=False,
+#         autoincrement=True,
+#     )
+#     user_id: Mapped[int] = mapped_column(Integer, ForeignKey("user.id"),  nullable=False, unique=False)
     
-    # amount: Mapped[int] = mapped_column(Integer, default=0)
-    action: Mapped[RecordActionType] = mapped_column(
-        Enum(
-            *get_args(RecordActionType),
-            name="recordActionType",
-            create_constraints=True,
-            validate_strins=True,
-            default="GET",
-        )
-    )
-    table: Mapped[TableType] = mapped_column(
-        Enum(
-            *get_args(TableType),
-            name="tableType",
-            create_constraints=True,
-            validate_strins=True,
-            default="USER",
-        )
-    )
+#     # amount: Mapped[int] = mapped_column(Integer, default=0)
+#     action: Mapped[RecordActionType] = mapped_column(
+#         Enum(
+#             *get_args(RecordActionType),
+#             name="recordActionType",
+#             create_constraints=True,
+#             validate_strins=True,
+#             default="GET",
+#         )
+#     )
+#     table: Mapped[TableType] = mapped_column(
+#         Enum(
+#             *get_args(TableType),
+#             name="tableType",
+#             create_constraints=True,
+#             validate_strins=True,
+#             default="USER",
+#         )
+#     )
     
-    table_id: Mapped[int] = mapped_column(Integer, nullable=False)
+#     table_id: Mapped[int] = mapped_column(Integer, nullable=False)
     
-    created_at: Mapped[DateTime] = mapped_column(DateTime, default=datetime.now)
-    updated_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now, onupdate=datetime.now)
-    custom_logs: Mapped[Optional[dict]] = mapped_column(JSON)
+#     created_at: Mapped[DateTime] = mapped_column(DateTime, default=datetime.now)
+#     updated_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now, onupdate=datetime.now)
+#     custom_logs: Mapped[Optional[dict]] = mapped_column(JSON)
 
-    user = relationship("UserModel", back_populates="record")
+#     user = relationship("UserModel", back_populates="record")
 
-    def __repr__(self) -> str:
-        return f"<RecordModel by {self.id} owned by={self.user_id}>"
+#     def __repr__(self) -> str:
+#         return f"<RecordModel by {self.id} owned by={self.user_id}>"
