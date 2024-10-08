eb9e8d54532db3a2cfee55a2f777e8d431bfd01d
wonderwall-tim <timothy.chow@wonderwall-g.gmail.com>
2024-10-08 12:15:02 +0900

    put method of dailyCheckIn

--- app/activity/api/v1/activity.py
+++ app/activity/api/v1/activity.py
@@ -38,7 +38,7 @@ def update_activity(db: Session=Depends(get_db)):
     """Update Activity"""
     return service.update_activity_logged_in(db) 
 
-@router.post('/daily-check-in', response_model=schemas.DailyCheckInResponseSchema)
+@router.put('/daily-check-in', response_model=schemas.DailyCheckInResponseSchema)
 def daily_check_in(request: schemas.DailyCheckInRequestSchema, db: Session=Depends(get_db)):
     """Daily check in for user"""
     return service.daily_check_in(request, db) 
